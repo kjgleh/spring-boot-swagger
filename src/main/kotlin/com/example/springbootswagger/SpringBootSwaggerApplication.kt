d0ed544390d3ddae1500294640e3944863dfe143
김정근 <gogoogjg@tripstore.kr>
2023-11-23 18:20:09 +0900

    set up project

---
+++ src/main/kotlin/com/example/springbootswagger/SpringBootSwaggerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootswagger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootSwaggerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootSwaggerApplication>(*args)
+}
