c74d01a3d96c98cf81801744261070a4ef518054
김정근 <gogoogjg@tripstore.kr>
2023-12-04 18:18:34 +0900

    스웨거 인증 추가

---
+++ src/main/kotlin/com/example/springbootswagger/common/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.springbootswagger.common
+
+object Constant {
+    const val BEARER_AUTH = "Bearer Auth"
+}
--- src/main/kotlin/com/example/springbootswagger/member/MemberController.kt
+++ src/main/kotlin/com/example/springbootswagger/member/MemberController.kt
@@ -1,11 +1,14 @@
 package com.example.springbootswagger.member
 
+import com.example.springbootswagger.common.Constant.BEARER_AUTH
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MemberController {
 
+    @SecurityRequirement(name = BEARER_AUTH)
     @GetMapping("/members")
     fun listup(): String {
         return "회원 목록 페이지"
