d199df7665082afded748be6d53678bbf6ecf288
김정근 <gogoogjg@tripstore.kr>
2024-07-17 14:43:17 +0900

    RequestHeader 에 Authorization 파라미터 추가

--- src/main/kotlin/com/example/springbootswagger/member/MemberController.kt
+++ src/main/kotlin/com/example/springbootswagger/member/MemberController.kt
@@ -3,6 +3,7 @@ package com.example.springbootswagger.member
 import com.example.springbootswagger.common.Constant.BEARER_AUTH
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -10,7 +11,9 @@ class MemberController {
 
     @SecurityRequirement(name = BEARER_AUTH)
     @GetMapping("/members")
-    fun listup(): String {
-        return "회원 목록 페이지"
+    fun listup(
+        @RequestHeader("Authorization") authorization: String,
+    ): String {
+        return "회원 목록 페이지($authorization)"
     }
 }
